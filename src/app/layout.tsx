0baf414282f2e7a4431b47550f5f67ff06be6092
Hassan <hassanjrao@gmail.com>
2026-07-29 01:34:34 +0500

    reniv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -149,7 +149,6 @@ export default async function RootLayout({
         </NextIntlClientProvider>
         <Analytics />
         <SpeedInsights />
-        <script src="https://ai-caller.hassanrao.com/widget.js" data-key="pk_kktLaZTPE8aMJlQRGrvALUoMz630kttUxo9Yy2eh" async></script>
       </body>
     </html>
   );
